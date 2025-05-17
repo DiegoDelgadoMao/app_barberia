069b0b53456c60891245378b2356f60d11cebef1
diegomao <diegomcode@gmail.com>
2025-05-17 11:14:15 -0500

    se añade corrige error en authController

--- backend/app-movil/src/main/java/com/corhuila/app_movil_g2/Controller/AuthController.java
+++ backend/app-movil/src/main/java/com/corhuila/app_movil_g2/Controller/AuthController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 import java.util.List;
 import java.util.stream.Collectors;
